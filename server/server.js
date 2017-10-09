1931c1acc0e51b2e59b8b0d6fb74ca8dcdd7e586
ahmed bham <ahmed.bham@gmail.com>
2017-10-09 08:55:06 -0400

    new message and create message events

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
 app.use(express.static(publicPath))
 const port = process.env.PORT || 3000
+//
+// io.connect({'forceNew': true})
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new connection made')
+
+socket.emit('newMessage', {
+  from: 'ahmed@gmail.com',
+  text: 'hey, how  ru ',
+  createdAt: 123
+})
+
+socket.on('createMessage', (newMessage) => {
+  newMessage.createdAt = 234
+  console.log('New message', newMessage)
+})
+
+socket.on('disconnect', () => {
+  console.log('client is disconnected')
+})
+})
+server.listen(port, () => {
   console.log(`running on port ${port}`)
 })
