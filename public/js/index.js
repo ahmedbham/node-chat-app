61fd8df891c39f3f19a677648215f6b0f87bdb44
ahmed bham <ahmed.bham@gmail.com>
2017-10-15 03:53:53 -0400

    sdd scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io()
 
+function scrollToBottom () {
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+  var clientHeight = messages.prop('clientHeight')
+  var scrollTop = messages.prop('scrollTop')
+  var scrollHeight = messages.prop('scrollHeight')
+  var newMessageHeight = newMessage.innerHeight()
+  var lastMessageHeight = newMessage.prev().innerHeight()
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
   console.log('connected to server')
 })
@@ -17,6 +31,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   })
   jQuery('#messages').append(html)
+  scrollToBottom()
 
   // console.log('new message arrived',message)
   // var formattedTime = moment(message.createdAt).format('h:mm a')
@@ -39,6 +54,7 @@ socket.on('newLocationMessage', function (message) {
   // a.attr('href', message.url)
   // li.append(a)
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 var messageTextbox = jQuery('[name=message]')
